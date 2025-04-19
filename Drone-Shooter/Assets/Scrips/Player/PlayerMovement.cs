ada60b9b6b15dee9820ddeb814b722ebfa1778a4
Martin <martinconcetti97@gmail.com>
2025-04-19 19:28:43 -0300

    Pool instantiates objects as child

--- Drone-Shooter/Assets/Scrips/Player/PlayerMovement.cs
+++ Drone-Shooter/Assets/Scrips/Player/PlayerMovement.cs
@@ -12,53 +12,21 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float maxSpeed;
 
 
-    [SerializeField] private float speed = 10;
-
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float groundDistance = 0.5f;
-
-    [SerializeField] private float crouchYScale;
-    [SerializeField] private float startYScale;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void Start()
-    {
-        startYScale = transform.localScale.y;
-    }
     private void FixedUpdate() 
     {
         Movement();
         VerticalMovement();
-
-
     }
 
 
     private void Movement()
     {
-        //float z = Input.GetAxis("Vertical");     
-        //float x = Input.GetAxis("Horizontal");  
-        //float y = 0;
-
-        //if (Mathf.Abs(z) < 0.01f && Mathf.Abs(x) < 0.01f)
-        //    return;
-
-        //Vector3 forward = transform.forward;
-        //Vector3 right = transform.right;
-
-        //forward.y = 0;
-        //right.y = 0;
-
-        //forward.Normalize();
-        //right.Normalize();
-
-        //Vector3 direction = (forward * z + right * x).normalized;
-        //rigidbody.AddForce(direction * speed, ForceMode.Acceleration);
-
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
