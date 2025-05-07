4b51291384dbe3161216c3164111cb91016c7b0b
Martin <martinconcetti97@gmail.com>
2025-05-07 05:44:54 -0300

    Lazer sight can be turn on and off

--- Drone-Shooter/Assets/Scrips/Weapons/Aim/LaserAim.cs
+++ Drone-Shooter/Assets/Scrips/Weapons/Aim/LaserAim.cs
@@ -5,31 +5,38 @@ using UnityEngine;
 public class LaserAim : MonoBehaviour
 {
     private LineRenderer lr;
-
+    private bool laserState;
+    private float range = 10;
     void Start()
     {
         lr = GetComponent<LineRenderer>();   
+        laserState = true;
     }
 
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            lr.enabled = !lr.enabled;
+        }
+
         RaycastHit hit;
-        if(Physics.Raycast(transform.position,transform.forward, out hit))
+        if(Physics.Raycast(transform.position,transform.forward, out hit, range))
         {
             if (hit.collider)
             {
-                //lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, 50));
-                //lr.SetPosition(1, new Vector3(transform.position.x,transform.position.y,hit.distance));
+                
                 lr.SetPosition(0, transform.position);
-                lr.SetPosition(1, transform.position + (transform.forward * 3));
+                //lr.SetPosition(1, transform.position + (transform.forward * 3));
+                lr.SetPosition(1,hit.point);
+
             }
         }
         else
         {
-            //lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, 50));
-            //lr.SetPosition(1, new Vector3(transform.position.x, transform.position.y, hit.distance));
+            
             lr.SetPosition(0, transform.position);
-            lr.SetPosition(1, transform.position + (transform.forward * 3));
+            lr.SetPosition(1, transform.position + (transform.forward * range));
         }
     }
 }
